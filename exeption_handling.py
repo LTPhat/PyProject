695ac4d9cb079ec31a39acab44cadec555106821
LTPhat <102889533+LTPhat@users.noreply.github.com>
2022-07-05 15:24:31 +0700

    Delete exeption_handling.py

--- exeption_handling.py
+++
@@ -1,9 +0,0 @@
-try:
-    numerator = int(input("Enter the number to divide: "))
-    denominator = int(input("Enter the number to divide by: "))
-    result = (numerator)/(denominator)
-    print(result)
-except ZeroDivisionError:
-    print("You can't devide by zero!!")
-except ValueError:
-    print("You must enter number")
